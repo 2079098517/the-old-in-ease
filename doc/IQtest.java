bb67868bd84e383b272919faf135edf9c35b6d22
anybaC <149356845+anybaC@users.noreply.github.com>
2023-11-03 09:21:10 +0800

    Add files via upload

---
+++ doc/IQtest.java
@@ -0,0 +1,14 @@
+package com.example.oldmansystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IQtest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iqtest);
+    }
+}
